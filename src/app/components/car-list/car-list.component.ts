55437dbb779fa7e858d6e89404b490e80d95a706
abrambarrientos <abrahambarrientos846@gmail.com>
2025-05-09 16:14:38 -0600

    feat: update Carro interface to include additional properties and add SafeUrlPipe implementation
    - Enhanced Carro interface with optional properties: imagenesAdicionales, video, and descripcionTecnica.
    - Added SafeUrlPipe for URL sanitization with a basic implementation and corresponding unit test.

--- src/app/components/car-list/car-list.component.ts
+++ src/app/components/car-list/car-list.component.ts
@@ -2,7 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Car } from '../../models/car.model';
 
 
--- src/app/interfaces/carro.interface.ts
+++ src/app/interfaces/carro.interface.ts
@@ -1,11 +1,14 @@
 export interface Carro {
-    id: number;
-    marca : string;
-    nombre: string;
-    imagen: string;
-    caracteristicasIzq: string[];
-    caracteristicasDer: string[];
-  }
-  export interface ApiResponse {
+  id: number;
+  marca: string;
+  nombre: string;
+  imagen: string;
+  caracteristicasIzq: string[];
+  caracteristicasDer: string[];
+  imagenesAdicionales?: string[];
+  video?: string; 
+  descripcionTecnica?: string[];
+}
+export interface ApiResponse {
   products: Carro[];
 }
